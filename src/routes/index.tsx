83a9eef5b873a0289e7fa6a0ce59186486236d77
Felipe Jung <felipemattoseu@gmail.com>
2025-04-06 18:18:20 -0400

    Implement notification display for OneSignal in the app routes and refactor Notification component for improved formatting

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,17 +1,58 @@
-import { useTheme } from 'native-base';
-import { DefaultTheme, NavigationContainer } from '@react-navigation/native';
+import { useEffect, useState } from "react";
+import { useTheme } from "native-base";
+import { DefaultTheme, NavigationContainer } from "@react-navigation/native";
+import {
+  NotificationWillDisplayEvent,
+  OneSignal,
+  OSNotification,
+} from "react-native-onesignal";
 
-import { AppRoutes } from './app.routes';
+import { Notification } from "../components/Notification";
+
+import { AppRoutes } from "./app.routes";
 
 export function Routes() {
   const { colors } = useTheme();
 
+  const [notification, setNotification] = useState<OSNotification | null>(null);
+
   const theme = DefaultTheme;
   theme.colors.background = colors.gray[700];
+
+  useEffect(() => {
+    const handleReceiveNotifications = (
+      event: NotificationWillDisplayEvent
+    ) => {
+      event.preventDefault();
+
+      const response = event.getNotification();
+
+      setNotification(response);
+    };
+
+    OneSignal.Notifications.addEventListener(
+      "foregroundWillDisplay",
+      handleReceiveNotifications
+    );
+
+    return () => {
+      OneSignal.Notifications.removeEventListener(
+        "foregroundWillDisplay",
+        handleReceiveNotifications
+      );
+    };
+  }, []);
 
   return (
     <NavigationContainer theme={theme}>
       <AppRoutes />
+
+      {notification && (
+        <Notification
+          title={notification.title || ""}
+          onClose={() => setNotification(null)}
+        />
+      )}
     </NavigationContainer>
   );
 }
